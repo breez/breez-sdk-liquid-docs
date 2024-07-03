86fae162ea6838ea573ffb0bf84d68226c22d626
ok300 <106775972+ok300@users.noreply.github.com>
2024-07-03 17:05:35 +0200

    Update Rust snippets with payment limits (#2)
    * Bump breez-sdk-liquid, update rust snippets
    
    * Remove go, csharp, python instructions
    
    * Update Rust snippets with limits
    
    * CI: bump SDK revision

--- snippets/rust/src/receive_payment.rs
+++ snippets/rust/src/receive_payment.rs
@@ -2,21 +2,26 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use breez_liquid_sdk::prelude::*;
+use log::info;
 
 async fn receive_payment(sdk: Arc<LiquidSdk>) -> Result<()> {
     // ANCHOR: receive-payment
-    // Set the amount you wish the payer to send
+    // Fetch the Receive limits
+    let current_limits = sdk.fetch_lightning_limits().await?;
+    info!("Minimum amount: {} sats", current_limits.receive.min_sat);
+    info!("Maximum amount: {} sats", current_limits.receive.max_sat);
+
+    // Set the amount you wish the payer to send, which should be within the above limits
     let prepare_receive_response = sdk
         .prepare_receive_payment(&PrepareReceiveRequest {
             payer_amount_sat: 5_000,
-        }).await?;
+        })
+        .await?;
 
     // If the fees are acceptable, continue to create the Receive Payment
     let receive_fees_sat = prepare_receive_response.fees_sat;
 
-    let receive_payment_response = sdk
-        .receive_payment(&prepare_receive_response)
-        .await?;
+    let receive_payment_response = sdk.receive_payment(&prepare_receive_response).await?;
 
     let invoice = receive_payment_response.invoice;
     // ANCHOR_END: receive-payment
