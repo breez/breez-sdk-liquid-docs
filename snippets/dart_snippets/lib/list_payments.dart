7f9ebcf5f3eb105cfb9621f33c18b758541fa36d
ok300 <106775972+ok300@users.noreply.github.com>
2024-07-03 16:11:17 +0200

    Remove listPaymentsFiltered

--- snippets/dart_snippets/lib/list_payments.dart
+++ snippets/dart_snippets/lib/list_payments.dart
@@ -9,28 +9,3 @@ Future<List<Payment>> listPayments() async {
   // ANCHOR_END: list-payments
   return paymentsList;
 }
-
-Future<List<Payment>> listPaymentsFiltered({
-  int? fromTimestamp,
-  int? toTimestamp,
-  bool? includeFailures,
-  int? offset,
-  int? limit,
-}) async {
-  // ANCHOR: list-payments-filtered
-  /// Get the desired epoch timestamp in seconds
-  int fromTimestamp = DateTime.now().subtract(const Duration(minutes: 30)).millisecondsSinceEpoch ~/ 1000;
-
-  ListPaymentsRequest req = ListPaymentsRequest(
-    filters: [PaymentTypeFilter.Sent],
-    fromTimestamp: fromTimestamp,
-    toTimestamp: toTimestamp,
-    includeFailures: includeFailures,
-    offset: offset,
-    limit: limit,
-  );
-  List<Payment> paymentsList = await breezSDK.listPayments(req: req);
-  print(paymentsList);
-  // ANCHOR_END: list-payments-filtered
-  return paymentsList;
-}
