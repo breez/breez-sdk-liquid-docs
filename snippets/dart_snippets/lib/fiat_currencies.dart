7d685e987292784da66dcf785cec6a42f7e7251b
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-07-05 12:21:53 +0300

    [Dart] Update Fiat Currency snippets

--- snippets/dart_snippets/lib/fiat_currencies.dart
+++ snippets/dart_snippets/lib/fiat_currencies.dart
@@ -3,14 +3,18 @@ import 'package:flutter_breez_liquid/flutter_breez_liquid.dart';
 
 Future<List<FiatCurrency>> listFiatCurrencies() async {
   // ANCHOR: list-fiat-currencies
-  List<FiatCurrency> fiatCurrencyList = await breezSDK.listFiatCurrencies();
+  List<FiatCurrency> fiatCurrencyList = await breezLiquidSDK.instance!.listFiatCurrencies();
   // ANCHOR_END: list-fiat-currencies
   return fiatCurrencyList;
 }
 
-Future<Map<String, Rate>> fetchFiatRates(String lspId) async {
+Future<Map<String, Rate>> fetchFiatRates() async {
   // ANCHOR: fetch-fiat-rates
-  Map<String, Rate> fiatRatesMap = await breezSDK.fetchFiatRates();
+  final List<Rate> rates = await breezLiquidSDK.instance!.fetchFiatRates();
+  final fiatRatesMap = rates.fold<Map<String, Rate>>({}, (map, rate) {
+    map[rate.coin] = rate;
+    return map;
+  });
   // print your desired rate
   print(fiatRatesMap["USD"]?.value);
   // ANCHOR_END: fetch-fiat-rates
