fdcc90e1ec961dfd80f18db8d518d92298828be1
Daniel Granhão <daniel.granhao@gmail.com>
2024-12-16 14:55:04 +0000

    Fix react native linting errors

--- snippets/react-native/parsing_inputs.ts
+++ snippets/react-native/parsing_inputs.ts
@@ -1,6 +1,6 @@
 import {
   InputTypeVariant,
-  parse,
+  parse
 } from '@breeztech/react-native-breez-sdk-liquid'
 
 const parseInputs = async () => {
@@ -8,34 +8,34 @@ const parseInputs = async () => {
   const input = 'an input to be parsed...'
 
   const parsed = await parse(input)
-  
+
   switch (parsed.type) {
     case InputTypeVariant.BITCOIN_ADDRESS:
       console.log(`Input is Bitcoin address ${parsed.address.address}`)
       break
-      
+
     case InputTypeVariant.BOLT11:
       console.log(
         `Input is BOLT11 invoice for ${
-          parsed.invoice.amountMsat 
-            ? parsed.invoice.amountMsat.toString() 
+          parsed.invoice.amountMsat != null
+            ? parsed.invoice.amountMsat.toString()
             : 'unknown'
         } msats`
       )
       break
-      
+
     case InputTypeVariant.LN_URL_PAY:
       console.log(
         `Input is LNURL-Pay/Lightning address accepting min/max ${parsed.data.minSendable}/${parsed.data.maxSendable} msats`
       )
       break
-      
+
     case InputTypeVariant.LN_URL_WITHDRAW:
       console.log(
         `Input is LNURL-Withdraw for min/max ${parsed.data.minWithdrawable}/${parsed.data.maxWithdrawable} msats`
       )
       break
-      
+
     default:
       // Other input types are available
       break
