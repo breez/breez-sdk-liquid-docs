b50fd264fb1028460b2371ce53bb4e9d448f0afb
Daniel Granhão <daniel.granhao@gmail.com>
2025-01-13 11:56:20 +0000

    Drop use of python match case

--- snippets/python/src/parsing_inputs.py
+++ snippets/python/src/parsing_inputs.py
@@ -7,19 +7,18 @@ def parse_input(sdk: BindingLiquidSdk):
 
     try:
         parsed_input = sdk.parse(input)
-        match parsed_input:
-            case InputType.BITCOIN_ADDRESS:
-                logging.debug(f"Input is Bitcoin address {parsed_input.address.address}")
-            case InputType.BOLT11:
-                amount = "unknown"
-                if parsed_input.invoice.amount_msat:
-                    amount = str(parsed_input.invoice.amount_msat)
-                logging.debug(f"Input is BOLT11 invoice for {amount} msats")
-            case InputType.LN_URL_PAY:
-                logging.debug(f"Input is LNURL-Pay/Lightning address accepting min/max {parsed_input.data.min_sendable}/{parsed_input.data.max_sendable} msats")
-            case InputType.LN_URL_WITHDRAW:
-                logging.debug(f"Input is LNURL-Withdraw for min/max {parsed_input.data.min_withdrawable}/{parsed_input.data.max_withdrawable} msats")
-            # Other input types are available
+        if parsed_input.type == InputType.BITCOIN_ADDRESS:
+            logging.debug(f"Input is Bitcoin address {parsed_input.address.address}")
+        elif parsed_input.type == InputType.BOLT11:
+            amount = "unknown"
+            if parsed_input.invoice.amount_msat:
+                amount = str(parsed_input.invoice.amount_msat)
+            logging.debug(f"Input is BOLT11 invoice for {amount} msats")
+        elif parsed_input.type == InputType.LN_URL_PAY:
+            logging.debug(f"Input is LNURL-Pay/Lightning address accepting min/max {parsed_input.data.min_sendable}/{parsed_input.data.max_sendable} msats")
+        elif parsed_input.type == InputType.LN_URL_WITHDRAW:
+            logging.debug(f"Input is LNURL-Withdraw for min/max {parsed_input.data.min_withdrawable}/{parsed_input.data.max_withdrawable} msats")
+        # Other input types are available
     except Exception as error:
         logging.error(error)
         raise
